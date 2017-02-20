a7c9b61dae5cdb3266163621c28ff38320c93e2f
xliuxu <xliuxu@gmail.com>
2017-02-20 19:27:31 +0800

    add step

--- ci/tasks/create-env-automation/deploy-cf.sh
+++ ci/tasks/create-env-automation/deploy-cf.sh
@@ -67,5 +67,7 @@ while IFS= read -r line; do
   ${deployment_dir}/bosh-cli* -e bosh-test upload-stemcell $line 
 done <<< "$stemcell"
 
+${deployment_dir}/bosh-cli-2.0.5-linux-amd64 -e bosh-test -d ${deploy_name} deploy ${deployment_dir}/cf-deploy.yml 
+
 echo "done">cf-info/cf-info
 
