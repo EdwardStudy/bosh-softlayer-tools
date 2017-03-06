acae25664d720de76675c2a07b03952307b30804
xliuxu <xliuxu@gmail.com>
2017-03-07 00:57:27 +0800

    fix escape

--- ci/tasks/create-env-automation/update-dns.sh
+++ ci/tasks/create-env-automation/update-dns.sh
@@ -20,7 +20,7 @@ ip_ha=$(grep ha_proxy ${deployment_dir}/deployed-vms|awk '{print $4}')
 
 cat >$deployment_dir/update_dns.sh<<EOF
 cat >/tmp/update_dns.sql<<ENDSQL
-DO \$\$
+DO \\\$\\\$
 DECLARE new_id INTEGER;
 BEGIN
     INSERT INTO domains(name, type) VALUES('${domain1}', 'NATIVE');
@@ -31,13 +31,13 @@ BEGIN
     SELECT domains.id INTO new_id from domains where domains.name = '$domain2' ;
     INSERT INTO records(name, type, content, ttl, domain_id) VALUES('$domain2','SOA','localhost hostmaster@localhost 0 10800 604800 30', 300, new_id);
     INSERT INTO records(name, type, content, ttl, domain_id) VALUES('*.$domain2', 'A', '$ip_ha', 300,new_id);
-END\$\$;
+END\\\$\\\$;
 ENDSQL
 /var/vcap/packages/postgres/bin/psql -U postgres -d bosh -a -f /tmp/update_dns.sql
 EOF
 pushd run-utils
 echo "$director_ip" >ip_list
-./run.sh -s $deployment_dir/update_dns.sh -i ip_list -p c1oudc0w -a | tee update_dns.log
+./run.sh -s $deployment_dir/update_dns.sh -i ip_list -p c1oudc0wc | tee update_dns.log
 popd
 
 
