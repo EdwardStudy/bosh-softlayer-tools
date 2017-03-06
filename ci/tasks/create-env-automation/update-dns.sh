9e4962538213b03fe8e8612e2f076e32803f87d1
xliuxu <xliuxu@gmail.com>
2017-03-06 23:44:11 +0800

    add run scripts

--- ci/tasks/create-env-automation/update-dns.sh
+++ ci/tasks/create-env-automation/update-dns.sh
@@ -4,24 +4,23 @@ set -e -x
 source bosh-softlayer-tools/ci/tasks/utils.sh
 source /etc/profile.d/chruby.sh
 
-apt-get install -y expect
-
 deployment_dir="${PWD}/deployment"
 mkdir -p $deployment_dir
+pushd $deployment_dir
 
 tar -zxvf director-artifacts/director_artifacts.tgz -C ${deployment_dir}
 tar -zxvf cf-artifacts/cf_artifacts.tgz -C ${deployment_dir}
-
+tar -zxvf run-utils/run-utils.tgz -C run-utils/
+di_password=$(grep -w root ${deployment_dir}/director-detail|awk '{print $4}')
 deploy_name=$(${deployment_dir}/bosh-cli* int ${deployment_dir}/cf-deploy.yml --path /name)
 director_ip=$(awk '{print $1}' deployment/director-hosts)
 domain1="${deploy_name}.bluemix.net"
 domain2="${deploy_name}.mybluemix.net"
-
-di_password=$(grep -w root ${deployment_dir}/director-detail|awk '{print $4}')
 pg_password=$(${deployment_dir}/bosh-cli* int ${deployment_dir}/credentials.yml --path /PG_PASSWORD)
 ip_ha=$(grep ha_proxy ${deployment_dir}/deployed-vms|awk '{print $4}')
 
-cat >update_dns.sql<<EOF
+cat >update_dns.sh<<EOF
+cat >update_dns.sql<ENDSQL
 DO \$\$
 DECLARE new_id INTEGER;
 BEGIN
@@ -34,24 +33,10 @@ BEGIN
     INSERT INTO records(name, type, content, ttl, domain_id) VALUES('$domain2','SOA','localhost hostmaster@localhost 0 10800 604800 30', 300, new_id);
     INSERT INTO records(name, type, content, ttl, domain_id) VALUES('*.$domain2', 'A', '$ip_ha', 300,new_id);
 END\$\$;
+ENDSQL
+/var/vcap/packages/postgres/bin/psql -U postgres -d bosh -a -f update_dns.sql
 EOF
 
-/usr/bin/env expect<<EOF
-spawn scp -o StrictHostKeyChecking=no ./update_dns.sql root@$director_ip:/tmp
-expect "*?assword:*"
-exp_send "$di_password\r"
-expect eof
-EOF
+run-utils/run.sh -s apply_etc_hosts.sh -i <(echo $director_ip) -p c1oudc0w -a | tee update_dns.log
 
-/usr/bin/env expect<<EOF
-spawn ssh root@$director_ip <<ENDSSH
-export PGPASSWORD=${pg_password}
-/var/vcap/packages/postgres/bin/psql -U postgres -d bosh -a -f /tmp/update_dns.sql
-ENDSSH
-expect {
-    "continue" { send "yes\r"; exp_continue }
-    "assword:" { send "$di_password\r"; }
-}
-
-
-
+popd
