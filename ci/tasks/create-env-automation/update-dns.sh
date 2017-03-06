cd6dfdbc0a7d9067d1db877a0317090a1f042fa4
xliuxu <xliuxu@gmail.com>
2017-03-07 00:22:07 +0800

    add -y flag

--- ci/tasks/create-env-automation/update-dns.sh
+++ ci/tasks/create-env-automation/update-dns.sh
@@ -3,7 +3,7 @@ set -e -x
 
 source bosh-softlayer-tools/ci/tasks/utils.sh
 source /etc/profile.d/chruby.sh
-apt-get install expect 
+apt-get -y install expect 
 deployment_dir="${PWD}/deployment"
 mkdir -p $deployment_dir
 
